6e8142e05304e6f60f24f2d70d7896805cf00f8a
zakarya0roua <z.roua@edu.umi.ma>
2026-09-18 18:20:49 +0100

    terminer les fonction sont trier les app par ordre alph

--- projet.js
+++ projet.js
@@ -40,47 +40,60 @@ do{
     choix = Number(prompt("votre choix: "));
     switch(choix){
         case 1:
+            // Afficher le tableau de bord
             afficherTableauDeBord();
             break;
         case 2:
-            console.dir(apprenants, {depth: null});
+            // Afficher la liste des apprenants
+            for(let i = 0; i < apprenants.length; i++){
+               console.log(apprenants[i]);
+            }
             break;
         case 3:
+            // Ajouter un apprenant
             let id_new = Number(prompt("Entre votre ID: "));
             let nomComplet_new = prompt("Entre le nom complet: ");
             let ville_new = prompt("Entre le ville: ")
             console.log(ajouterApprenant(id_new, nomComplet_new, ville_new));
             break;
         case 4:
-            let ID_rechercher = Number(prompt("Entre id pour rechercher"))
+            // Consulter un apprenant par identifiant
+            let ID_rechercher = Number(prompt("Entre id pour rechercher: "))
             console.log(rechercherApprenant(ID_rechercher));
             break;
         case 5:
+            // Ajouter ou modifier le résultat d'une journée
             let idRes = Number(prompt("Entre ID pour result: "))
             let jourRes = Number(prompt("Entre jour pour result: "))
             let exercicesRes = Number(prompt("Entre exrecices pour result: "))
             let totalRes = Number(prompt("Entre total des exercice pour result: "))
-            let challengeRes = prompt("Entre challenge complet oui/non" ) === "oui";
+            let challengeRes = prompt("Entre challenge complet oui/non " ) === "oui";
             enregistrerResultat(idRes, jourRes, exercicesRes, totalRes, challengeRes)
             break;
         case 6:
+            // Rechercher un apprenant par nom
             let nom_recherche = prompt("Entre nom pour recherche: ")
             console.log(rechercherApprenant(nom_recherche));
             break;
         case 7:
+            // Filtrer les apprenants par niveau
             let filtration = prompt("Entre le niveau (Solide/ En progression/ À renforcer)")
             console.log(filtrerParNiveau(filtration));
             break;
         case 8:
+            // Trier les apprenants par progression décroissante
             console.log(trierParProgression());
             break;
         case 9:
+            // Trier les apprenants par ordre alphabétique
             console.log("9. Trier les apprenants par ordre alphabétique");
             break;
         case 0:
+            // pour quitter code
             console.log("0. Quitter");
             break;
         default:
+            // pour verifier nomber
             console.log("entrer un nomber entre 0 et 9");
             break;
     }
@@ -154,7 +167,9 @@ function ajouterApprenant(id, nomComplet, ville){
 
 
 function enregistrerResultat(id, jour, exercicesTermines, totalExercices, challengeTermine){
-    let appTrouve = 0;
+    let appTrouve = null;
+    let new_objet = {};
+
     for(let i = 0; i < apprenants.length; i++){
         if(apprenants[i].id === id){
             appTrouve = apprenants[i];
@@ -194,6 +209,8 @@ function enregistrerResultat(id, jour, exercicesTermines, totalExercices, challe
         });
         console.log("Journee ajoutee");
     }
+    return appTrouve;
+    
 }
 
 
@@ -237,7 +254,7 @@ function rechercherApprenant(rechercher){
     }
 
     for(let i = 0; i < apprenants.length; i++){
-        if((apprenants[i].nomComplet === rechercher) || apprenants[i].id === rechercher){
+        if((apprenants[i].nomComplet.includes(rechercher)) || apprenants[i].id === rechercher){
             return apprenants[i];
         }
     }
